db2ca824e47b5dea932b188c23d81df77825c4aa
RBertini78 <r.bertini@gmail.com>
2024-12-07 23:48:16 -0300

    continuação angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
+
 export class AppComponent {
-  title = 'ToDo';
+  public title: String = 'Minhas Tarefas';
+  public todos: Todo[] = [];
+
+  constructor(){
+    this.todos.push(new Todo(1,'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', false));
+  }
 }
